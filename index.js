ae7b728c8272d4ae4a5217fbf5895a0dce969807
Md Hasibur Rahman <hasib@MddeMacBook-Pro.local>
2025-08-29 19:45:11 +0800

    added call functionality

--- index.js
+++ index.js
@@ -1,1 +1,81 @@
-console.log("connected");
+// counting hearts
+
+const hearts = document.querySelectorAll(".card-heart");
+const heartCount = document.getElementById("heart-count");
+
+let count = 0;
+for (const heart of hearts) {
+  heart.addEventListener("click", function () {
+    count++;
+    heartCount.innerText = count;
+  });
+}
+
+const date = new Date().toLocaleTimeString();
+function callHandle(id) {
+  const coinElement = document.getElementById("coin-count");
+  let coinValue = parseInt(coinElement.innerText);
+
+  if (coinValue < 20) {
+    alert(
+      "Not enough coins to make a call! you required 20 coins to make a call"
+    );
+    return;
+  }
+
+  const callHistoryParent = document.getElementById("call-history-parent");
+  const name = document.getElementById(`name-${id}`).innerText;
+  const number = document.getElementById(`number-${id}`).innerText;
+
+  alert(`calling ${name} ${number}...`);
+  coinValue -= 20;
+  coinElement.innerText = coinValue;
+
+  const div = document.createElement("div");
+  div.innerHTML = `<div
+          class="flex justify-between items-center py-1 px-2 bg-gray-100 m-2 rounded-lg"
+        >
+          <div>
+            <h1 class="font-bold">${name}</h1>
+            <h1>${number}</h1>
+          </div>
+          <div>
+            <h1>${date}</h1>
+          </div>
+        </div>`;
+  callHistoryParent.appendChild(div);
+}
+document.getElementById("clr-btn").addEventListener("click", function () {
+  document.getElementById(call - history - parent).style.display = "none";
+
+  // while (parent.children.length > 1) {
+  //   parent.removeChild(parent.lastChild);
+});
+
+document.getElementById("call-btn1").addEventListener("click", function () {
+  callHandle("card1");
+});
+document.getElementById("call-btn2").addEventListener("click", function () {
+  callHandle("card2");
+});
+document.getElementById("call-btn3").addEventListener("click", function () {
+  callHandle("card3");
+});
+document.getElementById("call-btn4").addEventListener("click", function () {
+  callHandle("card4");
+});
+document.getElementById("call-btn5").addEventListener("click", function () {
+  callHandle("card5");
+});
+document.getElementById("call-btn6").addEventListener("click", function () {
+  callHandle("card6");
+});
+document.getElementById("call-btn7").addEventListener("click", function () {
+  callHandle("card7");
+});
+document.getElementById("call-btn8").addEventListener("click", function () {
+  callHandle("card8");
+});
+document.getElementById("call-btn9").addEventListener("click", function () {
+  callHandle("card9");
+});
